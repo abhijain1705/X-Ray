05e3ebd76683089d18a0edef2c112fdc4d03bb4f
abhijain1705 <abhijain3002@gmail.com>
2025-12-27 22:00:45 +0530

    done with initial frontend

---
+++ frontend/next.config.ts
@@ -0,0 +1,30 @@
+import type { NextConfig } from 'next';
+
+// Define the base Next.js configuration
+const baseConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.slingacademy.com',
+        port: ''
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
+        port: ''
+      },
+      {
+        protocol: 'https',
+        hostname: 'clerk.com',
+        port: ''
+      }
+    ]
+  },
+  transpilePackages: ['geist']
+};
+
+let configWithPlugins = baseConfig;
+
+const nextConfig = configWithPlugins;
+export default nextConfig;
---
+++ frontend/src/features/auth/components/sign-up-view.tsx
@@ -0,0 +1,68 @@
+import { buttonVariants } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+import { SignUp as ClerkSignUpForm } from '@clerk/nextjs';
+import { Metadata } from 'next';
+import Link from 'next/link';
+import { InteractiveGridPattern } from './interactive-grid';
+
+export const metadata: Metadata = {
+  title: 'X-Ray | Sign Up',
+  description: 'Sign Up page for authentication.'
+};
+
+export default function SignUpViewPage() {
+  return (
+    <div className='relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0'>
+      <Link
+        href='/examples/authentication'
+        className={cn(
+          buttonVariants({ variant: 'ghost' }),
+          'absolute top-4 right-4 hidden md:top-8 md:right-8'
+        )}
+      >
+        Sign Up
+      </Link>
+      <div className='bg-muted relative hidden h-full flex-col p-10 text-white lg:flex dark:border-r'>
+        <div className='absolute inset-0 bg-zinc-900' />
+        <div className='relative z-20 flex items-center text-lg font-medium'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            viewBox='0 0 24 24'
+            fill='none'
+            stroke='currentColor'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            className='mr-2 h-6 w-6'
+          >
+            <path d='M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3' />
+          </svg>
+          X-Ray
+        </div>
+        <InteractiveGridPattern
+          className={cn(
+            'mask-[radial-gradient(400px_circle_at_center,white,transparent)]',
+            'inset-x-0 inset-y-[0%] h-full skew-y-12'
+          )}
+        />
+        <div className='relative z-20 mt-auto'>
+          <blockquote className='space-y-2'>
+            <p className='text-lg'>
+              &ldquo;X-Ray helps me understand why my system behaved a certain
+              way instead of blindly chasing logs.&rdquo;
+            </p>
+          </blockquote>
+        </div>
+      </div>
+      <div className='flex h-full items-center justify-center p-4 lg:p-8'>
+        <div className='flex w-full max-w-md flex-col items-center justify-center space-y-6'>
+          <ClerkSignUpForm
+            initialValues={{
+              emailAddress: 'your_mail@example.com'
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
